0f83c92721e03400122fe5b46f243ea90410f4dc
Matthew <MatthewHepburn0@gmail.com>
2015-08-18 14:06:19 +0100

    Adds a chart showing the proportion of correct to incorrect answers by page.

--- FSM Prototype/Deploy/stats.js
+++ FSM Prototype/Deploy/stats.js
@@ -52,6 +52,51 @@ var data = {
         return pageData;
 
     },
+    getPageAnswersData: function(){
+        var questionList = ["select-states-1", "give-list-1", "give-list-2", "satisfy-list-1",
+                    "satisfy-list-2-vending-machine", "satisfy-list-4-2010-resit", "satisfy-definition-1",
+                    "give-list-3-cabbage-goat-wolf", "satisfy-list-3-tower-of-hanoi2", "satisfy-regex-1",
+                    "satisfy-regex-2"];
+        var pageData = []
+        for (var i = 0; i < questionList.length; i++){
+            var thisData = data.json.urls[questionList[i]];
+            if (thisData === undefined){
+                continue;
+            }
+            var totalAnswers;
+            var correctAnswers;
+            var incorrectAnswers;
+            if (thisData.hasOwnProperty("totalAnswers")){
+                totalAnswers = thisData.totalAnswers
+            } else {
+                totalAnswers = 0;
+            }
+            if (thisData.hasOwnProperty("correctAnswers")){
+                correctAnswers = thisData.correctAnswers
+            } else {
+                correctAnswers = 0;
+            }
+            incorrectAnswers = totalAnswers - correctAnswers
+            var thisPage = {"name":questionList[i],
+                            "correctAnswers": correctAnswers,
+                            "totalAnswers": totalAnswers,
+                            "incorrectAnswers": incorrectAnswers
+                            };
+            pageData.push(thisPage)
+        }
+        return pageData;
+    },
+    getMaxAnswersPerPage:function(){
+        var max = 0;
+        for (url in data.json.urls){
+            var answers = data.json.urls[url].totalAnswers;
+            if (answers > max) {
+                max = answers;
+            }
+        }
+        return max;
+
+    },
     getMaxVisitorPerDate:function(){
         var max = 0;
         for (date in data.json.dates){
@@ -71,7 +116,6 @@ var data = {
             }
         }
         return max;
-
     }
 }
 
@@ -136,7 +180,7 @@ var display = {
             .html("Number of Unique Visitors per Day")
             .attr("style", "width: " + (display.width - 200) + "px;");
     },
-    drawPageBarChart:function(){
+    drawPageVisitsBarChart:function(){
         // Draw title
         d3.select("#title")
             .html("Total Unique Visitors per Page")
@@ -184,7 +228,66 @@ var display = {
             .call(xAxis);
 
         d3.select("#x-axis-title")
-            .html("Number of Unique Visitors by Page")
+            .html("Number of Unique Visitors")
+            .attr("style", "width: " + (display.width - 200) + "px;");
+
+    },
+    drawPageAnswersBarChart:function(){
+        // Draw title
+        d3.select("#title")
+            .html("Correct vs Total Answers by Question")
+            .attr("style", "width: " + (display.width - 200) + "px;");
+
+        var max = data.getMaxAnswersPerPage()
+        var barMargin = 2;
+        var xMargin = 4;
+        d3.select("#canvas")
+            .html("")
+        pageData = data.getPageAnswersData()
+        var barHeight = 30
+        axisWidth = 20
+        var height = axisWidth + (barHeight + barMargin) * pageData.length
+        var chart = d3.select("#canvas");
+        chart
+            .attr("height", height);
+        var scale = d3.scale.linear()
+                    .domain([0, max])
+                    .range([0, display.width - 200]);
+
+        var bar = d3.select("#canvas").selectAll("g")
+                    .data(pageData)
+                .enter().append("g")
+                    .attr("transform", function(d, i) { return "translate(" + xMargin + "," + i * (barHeight + 2) + ")"; })
+                    .classed("bar", true);
+
+        bar.append("rect")
+            .attr("width", function(d) { return scale(d.totalAnswers) })
+            .attr("height", barHeight)
+            .classed("total", true)
+
+        bar.append("rect")
+                    .attr("width", function(d) { return scale(d.correctAnswers) })
+                    .attr("height", barHeight)
+
+
+
+        bar.append("text")
+            .attr("x", function(d) { return scale(d.totalAnswers) + 6; })
+            .attr("y", barHeight / 2)
+            .attr("dy", ".35em")
+            .classed("right-label", true)
+            .text(function(d) { return d.name; });
+
+        var xAxis = d3.svg.axis()
+                        .scale(scale)
+                        .orient("bottom");
+        chart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate("+ xMargin + "," + (height - axisWidth) + ")")
+            .call(xAxis);
+
+        d3.select("#x-axis-title")
+            .html("Number of <i class='blue'>Correct Answers</i> and <i class='red'>Incorrect Answers</i>")
             .attr("style", "width: " + (display.width - 200) + "px;");
 
     }
@@ -196,7 +299,7 @@ var control = {
             setTimeout(control.initialDraw, 100)
             return
         }
-        display.drawPageBarChart()
+        display.drawDateBarChart()
     }
 
 }
