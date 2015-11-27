5142acabf28591a465ccb92ae8c0be3159a4a5ad
Matthew Hepburn <MatthewHepburn0@gmail.com>
2015-11-27 12:45:59 +0000

    Changes to the build system to accomodate the deployment of multiple separate question playlists.

--- FSM Prototype/build.py
+++ FSM Prototype/build.py
@@ -9,29 +9,65 @@ import sys
 import subprocess
 import shutil
 
-def getDir():
-    currentDir = os.getcwd()
-    return os.path.join(currentDir, "Source")
+# Pass in the address of the JS and CSS files
+# Eg if fsm.js is at www.example.com/static/fsm.js then jsAddress would be http://www.example.com/static/
 
+# An absolute reference is used to allow questions to be moved to different addresses without issue
 
-if __name__ == "__main__":
-    # Load in question data
-    with open('questions.JSON') as data_file:
+# Different addresses are set if the files are being built for deployment
+
+addresses = {}
+sourceDir = ""
+deployDir = ""
+startDir = os.getcwd()
+
+def setAddresses(toDeploy):
+    # Pass in the address of the JS and CSS files
+    # Eg if fsm.js is at www.example.com/static/fsm.js then jsAddress would be http://www.example.com/static/
+
+    # An absolute reference is used to allow questions to be moved to different addresses without issue
+
+    # Different addresses are set if the files are being built for deployment
+    global addresses
+    if toDeploy:
+        addresses = {
+            "jsAddress": "http://homepages.inf.ed.ac.uk/s1020995/",
+            "cssAddress": "http://homepages.inf.ed.ac.uk/s1020995/",
+            "iconAddress": "http://homepages.inf.ed.ac.uk/s1020995/img/Icons/",
+            "imgAddress": "http://homepages.inf.ed.ac.uk/s1020995/img/"
+        }
+    else:
+        addresses = {
+            "jsAddress": "../",
+            "cssAddress": "../",
+            "iconAddress": "../img/Icons",
+            "imgAddress": "../img/"
+        }
+
+def setDirs():
+    global sourceDir
+    global deployDir
+    global startDir
+
+    sourceDir = os.path.join(startDir, "Source")
+    deployDir = os.path.join(startDir, "Deploy")
+
+def buildQuestionSet(jsonFilename, dirName, question_template, end_template):
+    global deployDir
+ 
+    # Load in the question data for this questionSet
+    os.chdir(startDir)
+    with open(jsonFilename) as data_file:
         data = json.load(data_file)
 
-    # Setup Jinja
-    templateLoader = jinja2.FileSystemLoader("Source")
-    templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True)
-    # Also add "lstrip_blocks=True" above if jinja2.7 or greater is available
-    templatesDir = getDir()  # TODO work out what this does/if it does anything
-    question_template = templateEnv.get_template("question.jinja")
-    index_template = templateEnv.get_template("index.jinja")
-    end_template = templateEnv.get_template("end.jinja")
-
     # Change to /Deploy directory
-    currentDir = os.getcwd()
-    os.chdir("Deploy")
-    deployDir = os.getcwd()
+    os.chdir(deployDir)
+    # Create a directory dirName if it doesn't exist:
+    if os.path.exists(dirName):
+        os.chdir(dirName)
+    else:
+        os.makedirs(dirName)
+        os.chdir(dirName)
 
     i = 1;
 
@@ -44,14 +80,15 @@ if __name__ == "__main__":
             "links": question["data-links"].replace("'","&apos;" ),
             "options": question["data-options"].replace("'","&apos;" ),
             "question": question["data-question"].replace("'","&apos;" ),
-            "title": "FSM - Question #" + str(question["question-number"])
+            "title": "FSM - Question #" + str(question["question-number"]),
+            "addresses": addresses
         }
         # Set previous/next urls
         # NB - question numbering begins at 1.
         if question["question-number"] > 1:
             variables["previous"] = "href='" + data[question["question-number"]-2]["filename"] + ".html'"
         else:
-            variables["previous"] = "href='index.html'"
+            variables["previous"] = "href='../index.html'"
 
         if question["question-number"] < len(data):
             variables["next"] = "href='" + data[question["question-number"]]["filename"] + ".html'"
@@ -81,14 +118,6 @@ if __name__ == "__main__":
             f.write(outputText.encode("UTF-8"))
         f.close()
 
-    # Output index.html
-    variables = {"q1": data[0]["filename"] + ".html"}
-    outputText = index_template.render(variables)
-    f = open("index.html", "w")
-    f.write(outputText)
-    f.close()
-    print("index.html")
-
     # Output end.html
     variables = {"lastq": lastQuestion + ".html"}
     outputText = end_template.render(variables)
@@ -97,8 +126,52 @@ if __name__ == "__main__":
     f.close()
     print("end.html")
 
+
+if __name__ == "__main__":
+
+    # Check for deployment flag
+    toDeploy = False
+    for arg in sys.argv:
+        if arg in ["-d", "-D", "--Deploy", "--deploy"]:
+            toDeploy = True
+            break
+    # Set the address global
+    setAddresses(toDeploy) 
+
+    # Set up the local directory globals
+    setDirs()
+
+    # Load in the list of question files - which contains the filename of each question set's json file.
+    with open('questionFiles.json') as data_file:
+        data = json.load(data_file)
+
+    # Setup Jinja
+    templateLoader = jinja2.FileSystemLoader("Source")
+    templateEnv = jinja2.Environment(loader=templateLoader, trim_blocks=True)
+    # Also add "lstrip_blocks=True" above if jinja2.7 or greater is available
+    templatesDir = sourceDir  # TODO work out what this does/if it does anything
+    question_template = templateEnv.get_template("question.jinja")
+    index_template = templateEnv.get_template("index.jinja")
+    end_template = templateEnv.get_template("end.jinja")
+
+    for questionSet in data:
+        buildQuestionSet(questionSet["file"], questionSet["directory"], question_template, end_template)
+
+    #Return to deploy directory
+    os.chdir(deployDir)   
+
+    # Output index.html
+    variables = {"q1": "inf1/demo1-intro-to-fsm" + ".html"}
+    outputText = index_template.render(variables)
+    f = open("index.html", "w")
+    f.write(outputText)
+    f.close()
+    print("index.html")
+
+
+
     # Return to previous directory.
-    os.chdir(currentDir)
+    os.chdir(startDir)
 
     # Minify css and JS if requested:
     minifyRequested = False
@@ -107,7 +180,7 @@ if __name__ == "__main__":
             minifyRequested = True
             break
     if minifyRequested:
-        os.chdir("Source")
+        os.chdir(sourceDir)
         sourceDir = os.getcwd()
         # Arguement to listdir is optional in python 3.2+ but used here for compatability with DICE
         jsFiles = [ f for f in os.listdir(sourceDir) if f[-3:] == ".js"]
@@ -127,7 +200,7 @@ if __name__ == "__main__":
             outfile.close()
     else:
         # Copy JS and CSS unaltered
-        os.chdir("Source")
+        os.chdir(sourceDir)
         sourceDir = os.getcwd()
         files = [f for f in os.listdir(sourceDir) if f[-4:] == ".css" or f[-3:] == ".js"]
         for f in files:
@@ -140,7 +213,7 @@ if __name__ == "__main__":
         shutil.copy(f, deployDir)
 
     # Return to original directory.
-    os.chdir(currentDir)
+    os.chdir(startDir)
 
 
 
